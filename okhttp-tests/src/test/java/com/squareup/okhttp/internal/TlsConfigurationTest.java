c1b3fc47201f0bec5882017006d342f5e7653ef3
Neil Fuller <nfuller@google.com>
2014-10-28 13:59:09 +0000

    Proposed changes for dealing with more granular TLS connection fallback
    This change also enabled OkHttp to handle socket factories that produce
    sockets that do not not enable all the protocols that might be part of
    the fallback strategy. e.g. to avoid enabling SSLv3 when SSLv3 is not
    enabled by default.
    
    If a socket is encountered that does not support, e.g., TLSv1.2 the
    negotiation is not attempted or failed; the strategy simply falls back
    until it finds a TlsConfiguration that can work. If none will work an
    exception is thrown.
    
    The majority of this change is dealing with the fact that it is not easy
    to know in advance which TLS/SSL protocol versions are supported by a socket
    without having a socket instance. OkHttp currently assumes that TLS
    configuration can be predetermined.
    
    RouteSelector no longer deals with TLS fallback. This is now handled by the
    Connection instead.
    
    A later CL will modify the fallback strategy to try various TLS versions
    (when supported) instead of switching immediately to SSLv3.
    This change allows Android to switch with minimal code changes, at least
    until we take a newer version of OkHttp where it may be easier.
    
    Bug: 13228108
    Change-Id: If76b0b5699c22601f21c9afe494d7089ac2fa55c

---
+++ okhttp-tests/src/test/java/com/squareup/okhttp/internal/TlsConfigurationTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2014 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.okhttp.internal;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TlsConfigurationTest {
+
+  private static final SSLContext sslContext = SslContextBuilder.localhost();
+
+  @Test
+  public void sslV3Only() throws Exception {
+    SSLSocket compatibleSocket = createSocketWithEnabledProtocols("SSLv3", "TLSv1");
+    try {
+      assertTrue(TlsConfiguration.SSL_V3_ONLY.isCompatible(compatibleSocket));
+      TlsConfiguration.SSL_V3_ONLY.configureProtocols(compatibleSocket);
+      assertArrayEquals(new String[] { "SSLv3" }, compatibleSocket.getEnabledProtocols());
+    } finally {
+      compatibleSocket.close();
+    }
+
+    SSLSocket incompatibleSocket = createSocketWithEnabledProtocols("TLSv1");
+    try {
+      assertFalse(TlsConfiguration.SSL_V3_ONLY.isCompatible(incompatibleSocket));
+    } finally {
+      incompatibleSocket.close();
+    }
+
+    assertFalse(TlsConfiguration.SSL_V3_ONLY.supportsNpn());
+  }
+
+  @Test
+  public void useDefault() throws Exception {
+    String[] defaultProtocols = { "SSLv3", "TLSv1" };
+    SSLSocket socket = createSocketWithEnabledProtocols(defaultProtocols);
+
+    try {
+      assertTrue(TlsConfiguration.USE_DEFAULT.isCompatible(socket));
+      TlsConfiguration.USE_DEFAULT.configureProtocols(socket);
+      assertArrayEquals(defaultProtocols, socket.getEnabledProtocols());
+    } finally {
+      socket.close();
+    }
+
+    assertTrue(TlsConfiguration.USE_DEFAULT.supportsNpn());
+  }
+
+  private SSLSocket createSocketWithEnabledProtocols(String... protocols) throws IOException {
+    SSLSocket socket = (SSLSocket) sslContext.getSocketFactory().createSocket();
+    socket.setEnabledProtocols(protocols);
+    return socket;
+  }
+}
